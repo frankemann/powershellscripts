13146ea6b19f53ecb7dd109c55f1c4e1f64a9d22
frankemann <82226898+frankemann@users.noreply.github.com>
2021-04-29 15:38:08 +0200

    Add files via upload

---
+++ Deploy_Teamviewer_Host_With_Tvopt.ps1
@@ -0,0 +1,15 @@
+﻿# Install Teamviewer Host Setup .Msi from your
+# Teamviewer management console and combine it with your preferred default settings
+
+$PathToHost = "Type in your direct download url to the file"
+$PathToSettings = "Type in your direct download url to the file"
+
+If(!(test-path C:\TEMP\))
+{
+      New-Item -ItemType Directory -Force -Path C:\TEMP\
+}
+
+cd c:\temp\
+Invoke-WebRequest "$PathToHost" -Outfile Teamviewer_Host.msi
+Invoke-WebRequest "$PathToSettings" -Outfile default.tvopt
+Start-Process msiexec.exe -Wait -ArgumentList '/i C:\Temp\Teamviewer_Host.msi /qn SETTINGSFILE="C:\temp\default.tvopt" /quiet'
