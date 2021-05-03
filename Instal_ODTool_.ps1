e5d05d039bda743ee2715cc50ea9215053d83e61
frankemann <82226898+frankemann@users.noreply.github.com>
2021-05-03 10:20:28 +0200

    Add files via upload

---
+++ Instal_ODTool_.ps1
@@ -0,0 +1,22 @@
+﻿# Install Onedrive Monitoring Tool
+
+# Force use of TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$PathToFile1 = "Url to OneDriveLib.dll"
+$PathToFile2 = "Url to OneDriveLib.pdb"
+
+If(!(test-path C:\ODTool\))
+{
+      New-Item -ItemType Directory -Force -Path C:\ODTool\
+}
+
+cd c:\ODTool\
+
+# Download files
+Invoke-WebRequest "$PathToFile1" -Outfile OneDriveLib.dll
+Invoke-WebRequest "$PathToFile2" -Outfile OneDriveLib.pdb
+
+
+# Unblock the .dll file
+Unblock-File -Path C:\ODTool\OneDriveLib.dll
